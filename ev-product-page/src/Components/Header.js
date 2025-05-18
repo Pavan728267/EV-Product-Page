3ab134528b24b9c3672b1c64288ae867e47f944e
pavansj <pavan.kumar@riversilica.com>
2025-05-18 16:14:27 +0530

    Latest Frontend & Backend

--- ev-product-page/src/Components/Header.js
+++ ev-product-page/src/Components/Header.js
@@ -58,7 +58,7 @@ const Header = () => {
             {/* EV Scooters Dropdown */}
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#">EV Scooters </a>
-              <svg style={{ marginLeft: '6px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <svg style={{ marginLeft: '6px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
               </svg>
               <ul className="dropdown-menu">
@@ -73,7 +73,7 @@ const Header = () => {
             {/* Rooftop Solar Dropdown */}
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#">Rooftop Solar </a>
-               <svg style={{ marginLeft: '6px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+               <svg style={{ marginLeft: '6px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
               </svg>
               <ul className="dropdown-menu">
